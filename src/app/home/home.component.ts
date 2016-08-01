1d242bd272b057e7ee6591d9280ce9502ccefeb8
Hugo Ferrando Seage <hugoseage@gmail.com>
2016-08-01 14:53:52 +0200

    Simplified the Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,15 @@
 import { Component } from '@angular/core';
-
-import { AppState } from '../app.service';
-import { Title } from './title';
-import { XLarge } from './x-large';
-
 import { Intro } from '../intro';
 
 @Component({
-  selector: 'home',  // <home></home>
-  providers: [
-    Title
-  ],
-  directives: [
-    XLarge,
-    Intro
-  ],
+  selector: 'home',
+  providers: [],
+  directives: [ Intro ],
   pipes: [ ],
   styleUrls: [ './home.style.css' ],
   templateUrl: './home.template.html'
 })
+
 export class Home {
-  localState = { value: '' };
-  constructor(public appState: AppState, public title: Title) {}
-
-  ngOnInit() {
-    console.log('hello `Home` component');
-  }
-
-  submitState(value) {
-    console.log('submitState', value);
-    this.appState.set('value', value);
-    this.localState.value = '';
-  }
-
+  constructor() {}
 }
