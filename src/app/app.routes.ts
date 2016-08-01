b866b85ca020480ff8b6ba49b5907c0773ea3516
Hugo Ferrando Seage <hugoseage@gmail.com>
2016-08-01 12:05:45 +0200

    Added PGP section and link to blog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,16 +11,19 @@ export const routes: RouterConfig = [
   { path: 'home',  component: Home },
   { path: 'projects',  component: 'Projects' },
   { path: 'cv',  component: 'CV' },
+  { path: 'pgp',  component: 'PGP' },
   { path: '**',    component: NoContent }
 ];
 
 export const asyncRoutes: AsyncRoutes = {
   'Projects': require('es6-promise-loader!./projects'),
   'CV': require('es6-promise-loader!./cv')
+  'PGP': require('es6-promise-loader!./pgp')
 };
 
 export const prefetchRouteCallbacks: Array<IdleCallbacks> = [
   asyncRoutes['Projects'],
+  asyncRoutes['PGP'],
   asyncRoutes['CV']
 ];
 
---
+++ src/app/pgp/pgp.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'pgp',
+  styleUrls: [ './pgp.style.css' ],
+  templateUrl: './pgp.template.html'
+})
+
+export class PGP {
+  constructor(public route: ActivatedRoute) {
+
+  }
+
+}
